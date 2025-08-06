cd61a7940bdb94ddbee0d40ffd483277937912f2
Brett Loewen <brettzky007@gmail.com>
2025-08-06 16:21:47 -0500

    Datagen

---
+++ src/main/java/net/supremetor/tutorialmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,22 @@
+package net.supremetor.tutorialmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+import net.supremetor.tutorialmod.item.ModItems;
+import net.supremetor.tutorialmod.util.ModTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS)
+                .add(ModItems.PINK_GARNET)
+                .add(ModItems.RAW_PINK_GARNET);
+    }
+}
